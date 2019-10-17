946378da23a58d34ef27d09136d0f11656338ebd
태양 <56237265+taeyangcode@users.noreply.github.com>
2019-10-17 16:34:54 -0700

    Update Main.java

--- Emotion Game/Main.java
+++ Emotion Game/Main.java
@@ -1,1 +1,8 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String args[]) {
+        PlayerMsg playerGameTalk = new PlayerMsg();
+        playerGameTalk.playerGameMsg("MSG1", "MSG2");
+    }
+}
