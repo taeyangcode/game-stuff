ac3115c721c05934338399d3af671c56913a82bd
태양 <56237265+taeyangcode@users.noreply.github.com>
2019-10-17 23:19:25 -0700

    Create Timer.java

---
+++ Emotion Game/Timer.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Timer {
+    
+    
+    
+    public void oneSecond() {
+        try {
+            Thread.sleep(1000);
+        }
+        catch(InterruptedException e) {
+            System.out.println(e);
+        }
+    }
+    public void twoSecond() {
+        try {
+            Thread.sleep(2000);
+        }
+        catch(InterruptedException e) {
+            System.out.println(e);
+        }
+    }
+    public void threeSecond() {
+        try {
+            Thread.sleep(3000);
+        }
+        catch(InterruptedException e) {
+            System.out.println(e);
+        }
+    }
+    public void fourSecond() {
+        try {
+            Thread.sleep(4000);
+        }
+        catch(InterruptedException e) {
+            System.out.println(e);
+        }
+    }
+    
+}
