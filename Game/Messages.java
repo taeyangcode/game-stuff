ab27b8b6323f0ca7bc28cbb6873ef1408611e389
태양 <56237265+taeyangcode@users.noreply.github.com>
2019-10-21 22:30:46 -0700

    Update Messages.java

--- Game/Messages.java
+++ Game/Messages.java
@@ -4,10 +4,10 @@ public class Messages {
     Timer pause = new Timer();
     
     
-    public void playerMsg(String pTalkOne, String pTalkTwo) {
-        System.out.println(pTalkOne);
-        pause.twoSecond();
-        System.out.println(pTalkTwo);
+    public void playerMsg(ArrayList<String> list) {
+        for(String name: list) {
+            System.out.println(name);
+        }
     }
     public void gameMsg() {
         
