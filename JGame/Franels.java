1b2f1eedc36d3d38e429da4773bb037dc3122ac4
태양 <56237265+taeyangcode@users.noreply.github.com>
2019-10-29 09:57:34 -0700

    Update Franels.java

--- JGame/Franels.java
+++ JGame/Franels.java
@@ -4,9 +4,12 @@ import java.awt.*;
 public class Franels {
     int width, height;
     
+    Game taker;
+    
     JFrame frame = new JFrame("Title");
     JPanel panel = new JPanel();
     JButton button = new JButton("Hello");
+    JTextArea textArea = new JTextArea(taker.getAmount());
     
     public void JFrame(int width, int height) {
         this.width = width;
@@ -27,14 +30,19 @@ public class Franels {
         panel.setBackground(Color.blue);
         panel.setVisible(true);
         
-        panel.add(button);
+        //panel.add(button);
+        panel.add(textArea);
     }
     
-    public void JButton() {
-        button.setBounds(300, 300, 80, 30);
-        button.setBackground(Color.red);
-        button.setVisible(true);
+    public void JText() {
+        textArea.setEditable(false);
     }
+    
+    //public void JButton() {
+        //button.setBounds(300, 300, 80, 30);
+        //button.setBackground(Color.red);
+        //button.setVisible(true);
+    //}
     
     
     
