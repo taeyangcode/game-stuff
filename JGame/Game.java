83d0c143982b411d63de49e0a417fb0e68239773
태양 <56237265+taeyangcode@users.noreply.github.com>
2019-10-28 23:20:13 -0700

    Create Game.java

---
+++ JGame/Game.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Game extends MouseAdapter {
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("abc");
+    }
+    
+    
+    
+    
+    
+}
