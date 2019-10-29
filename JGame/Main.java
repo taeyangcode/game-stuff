4fa7e7a6160d166a9dcea505effba8cc3764e8f9
태양 <56237265+taeyangcode@users.noreply.github.com>
2019-10-28 23:19:50 -0700

    Create Main.java

---
+++ JGame/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    
+    
+    public static void main(String args[]) {
+        Franels Windows = new Franels();
+        Windows.JFrame(900, 900);
+        Windows.JPanel();
+        Windows.JButton();
+    }
+    
+    
+    
+}
