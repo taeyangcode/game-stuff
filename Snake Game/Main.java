c767f42b5cd2fed683fe8beae43409d92a246fdf
태양 <56237265+taeyangcode@users.noreply.github.com>
2019-12-05 16:44:18 -0800

    Create Main.java

---
+++ Snake Game/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) {
+        Game run = new Game();
+        run.swingSetup(600, 600);
+    }
+}
