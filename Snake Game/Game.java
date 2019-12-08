29808a81513e39ac28f7fe48d50017f2b481508b
태양 <56237265+taeyangcode@users.noreply.github.com>
2019-12-07 23:46:36 -0800

    Update Game.java

--- Snake Game/Game.java
+++ Snake Game/Game.java
@@ -1,21 +1,25 @@
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.*;
+import java.awt.event.KeyListener;
 import javax.swing.border.*;
-import java.util.concurrent.TimeUnit;
 
-public class Game {
+public class Game implements KeyListener {
     int gameRow = 15, gameCol = 15;
     int snakeX = 1, snakeY = 1;
-    int snakeHead, snakeBody;
     
+    Border gridBorder = new LineBorder(Color.BLACK, 1);
+    Border snakeBorder = new LineBorder(Color.decode("#228B22"), 4);
     GridLayout gameBoardLayout = new GridLayout(gameRow, gameCol);
     JFrame gameScreen = new JFrame();
     JPanel panelScreen = new JPanel();
+    JPanel endscreen = new JPanel();
     JLabel[][] gameLayout = new JLabel[gameRow][gameCol];
     
     public void swingSetup(int screenW, int screenH) {
+        gameScreen.addKeyListener(this);
+        
         gameScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gameScreen.setSize(screenW, screenH);
         gameScreen.setResizable(false);
@@ -27,90 +31,80 @@ public class Game {
         for(int i = 0; i < gameRow; i++) {
             for(int k = 0; k < gameCol; k++) {
                 gameLayout[i][k] = new JLabel();
-                gameLayout[i][k].setBorder(new LineBorder(Color.BLACK, 1));
+                gameLayout[i][k].setBorder(gridBorder);
                 gameLayout[i][k].setOpaque(true);
                 panelScreen.add(gameLayout[i][k]);
             }
         }
         
+        gameLayout[snakeY][snakeX].setBorder(snakeBorder);
+        
         gameScreen.setVisible(true);
     }
     
-    public void moveConfig(KeyEvent e) {
-        boolean moveUp = false;
-        boolean moveRight = false;
-        boolean moveDown = false;
-        boolean moveLeft = false;
-        
+    @Override
+    public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
     
         if(key == KeyEvent.VK_UP) {
-            moveUp = true;
-            moveRight = moveDown = moveLeft = false;
-            while(moveUp == true) {
-                snakeY++;
-                try {
-                    Thread.sleep(250);
-                }
-                catch(InterruptedException ex) {
-                    Thread.currentThread().interrupt();
-                }
-                if(key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_DOWN || key == KeyEvent.VK_LEFT) {
-                    moveUp = false;
-                }
+            System.out.println(snakeY);
+            snakeY--;
+            gameLayout[snakeY][snakeX].setBorder(snakeBorder);
+            
+            try {
+                Thread.sleep(1000);
+            }
+            catch(InterruptedException ex) {
+                Thread.currentThread().interrupt();
             }
         }
         else if(key == KeyEvent.VK_RIGHT) {
-            moveRight = true;
-            moveDown = moveLeft = moveUp = false;
-            while(moveRight == true) {
-                snakeX++;
-                try {
-                    Thread.sleep(250);
-                }
-                catch(InterruptedException ex) {
-                    Thread.currentThread().interrupt();
-                }
-                if(key == KeyEvent.VK_DOWN || key == KeyEvent.VK_LEFT || key == KeyEvent.VK_UP) {
-                    moveRight = false;
-                }
+            System.out.println(snakeX);
+            snakeX++;
+            gameLayout[snakeY][snakeX].setBorder(snakeBorder);
+            
+            try {
+                Thread.sleep(1000);
+            }
+            catch(InterruptedException ex) {
+                Thread.currentThread().interrupt();
             }
         }
         else if(key == KeyEvent.VK_DOWN) {
-            moveDown = true;
-            moveLeft = moveUp = moveRight = false;
-            while(moveDown == true) {
-                snakeY--;
-                try {
-                    Thread.sleep(250);
-                }
-                catch(InterruptedException ex) {
-                    Thread.currentThread().interrupt();
-                }
-                if(key == KeyEvent.VK_LEFT || key == KeyEvent.VK_UP || key == KeyEvent.VK_RIGHT) {
-                    moveDown = false;
-                }
+            System.out.println(snakeY);
+            snakeY++;
+            gameLayout[snakeY][snakeX].setBorder(snakeBorder);
+            
+            try {
+                Thread.sleep(1000);
+            }
+            catch(InterruptedException ex) {
+                Thread.currentThread().interrupt();
             }
         }
         else if(key == KeyEvent.VK_LEFT) {
-            moveLeft = true;
-            moveUp = moveRight = moveDown = false;
-            while(moveLeft == true) {
-                snakeX--;
-                try {
-                    Thread.sleep(250);
-                }
-                catch(InterruptedException ex) {
-                    Thread.currentThread().interrupt();
-                }
-                if(key == KeyEvent.VK_UP || key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_DOWN) {
-                    moveLeft = false;
-                }
+            System.out.println(snakeX);
+            snakeX--;
+            gameLayout[snakeY][snakeX].setBorder(snakeBorder);
+            
+            try {
+                Thread.sleep(1000);
+            }
+            catch(InterruptedException ex) {
+                Thread.currentThread().interrupt();
             }
         }
     }
     
-    public void snakeSetup() {
+    @Override 
+    public void keyReleased(KeyEvent e) {
+    }
     
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+    
+    public void snakeSetup() {
+        
     }
 }
