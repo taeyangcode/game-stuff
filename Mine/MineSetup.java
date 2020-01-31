f2008cc3a4b3bcdaf8e8d6a1e1aa5776247aae8f
태양 <56237265+taeyangcode@users.noreply.github.com>
2020-01-31 11:15:58 -0800

    Create MineSetup.java

---
+++ Mine/MineSetup.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class MineSetup {
+
+    /*
+    needed for mines:
+    size in the game,
+    what it produces,
+    how much it produces,
+    upgrades,
+    */
+
+    void mineDetails(String mineType, String productionType, int productionAmount, int mineWidth, int mineHeight) {
+
+    }
+}
