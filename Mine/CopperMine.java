4b3efee9b33cfbf68054f3d6113f7a95fe34e339
태양 <56237265+taeyangcode@users.noreply.github.com>
2020-02-04 15:34:51 -0800

    Update CopperMine.java

--- Mine/CopperMine.java
+++ Mine/CopperMine.java
@@ -2,35 +2,21 @@ package com.jetbrains;
 
 public class CopperMine {
 
-    MineSetup use;
-
     private int copperOneAmount = 0, copperTwoAmount = 0, copperThreeAmount = 0;
 
     void copperTierOne() {
         copperOneAmount++;
-        new MineSetup(nameIncreaserOne(), "Bronze Tier 1", 100, 1, 5000);
+        new MineSetup("B1" + copperOneAmount, 100, 1, 5000);
     }
 
     void copperTierTwo() {
         copperTwoAmount++;
-        new MineSetup(nameIncreaserTwo(), "Bronze Tier 2", 1000, 4, 10000);
+        new MineSetup("B2" + copperTwoAmount, 1000, 4, 10000);
     }
 
     void copperTierThree() {
         copperThreeAmount++;
-        new MineSetup(nameIncreaerThree(), "Bronze Tier 3", 15000, 10, 15000);
-    }
-
-    String nameIncreaserOne() {
-        return use.getMineName() + Integer.toString(copperOneAmount);
-    }
-
-    String nameIncreaserTwo() {
-        return use.getMineName() + Integer.toString(copperTwoAmount);
-    }
-
-    String nameIncreaerThree() {
-        return use.getMineName() + Integer.toString(copperThreeAmount);
+        new MineSetup("B3" + copperThreeAmount, 15000, 10, 15000);
     }
 
 }
