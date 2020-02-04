efd2fc9c206c3297f65a6eb1600b1eff81c3965f
태양 <56237265+taeyangcode@users.noreply.github.com>
2020-02-03 23:46:16 -0800

    Update MineSetup.java

--- Mine/MineSetup.java
+++ Mine/MineSetup.java
@@ -1,6 +1,13 @@
-package com.company;
+package com.jetbrains;
 
 public class MineSetup {
+
+    private String mineName;
+    private String mineType;
+
+    private int cost;
+    private int prodAmount;
+    private int prodTimeMilli;
 
     /*
     needed for mines:
@@ -10,8 +17,45 @@ public class MineSetup {
     upgrades,
     */
 
-    MineSetup(String mineType, String productionType, int productionAmount, int cost, int mineWidth, int mineHeight) {
+    MineSetup(String mineName, String mineType, int cost, int prodAmount, int prodTimeMilli) {
+    }
 
+    void setMineName(String mineName) {
+        this.mineName = mineName;
+    }
+
+    String getMineName() {
+        return mineName;
+    }
+
+    void setMineType(String mineType) {
+        this.mineType = mineType;
+    }
+
+    String getMineType() {
+        return mineType;
+    }
+
+    void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    int cost() {
+        return cost;
+    }
+
+    void setProdAmount(int prodAmount) {
+        this.prodAmount = prodAmount;
+    }
+
+    int getProdAmount() {
+        return prodAmount;
+    }
+
+    void setProdTime(int prodTimeMilli) { this.prodTimeMilli = prodTimeMilli; }
+
+    int getProdTime() {
+        return prodTimeMilli;
     }
 
 }
