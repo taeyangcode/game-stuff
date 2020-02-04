b4ba4fec66e1f9f8e5267904e6f365daeb8a26e0
태양 <56237265+taeyangcode@users.noreply.github.com>
2020-02-04 15:34:32 -0800

    Update MineralProduction.java

--- Mine/MineralProduction.java
+++ Mine/MineralProduction.java
@@ -2,7 +2,27 @@ package com.company;
 
 public class MineralProduction {
 
+    MineSetup test;
+
+    private int copperPrice = 1;
+    private int silverPrice = 10;
+    private int goldPrice = 100;
+
     void bronzeProduction() {
+        try {
+            Thread.sleep(test.getProdTime(test.getMineNumber()));
+            test.getProdAmount(test.getMineNumber());
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void silverProduction() {
+
+    }
+
+    void goldProduction() {
 
     }
 
