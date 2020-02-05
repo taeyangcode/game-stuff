4667c414cef594c2a911ce1ef8f450362a21ac50
태양 <56237265+taeyangcode@users.noreply.github.com>
2020-02-05 00:15:54 -0800

    Update and rename CopperMine.java to MineCreation.java

---
+++ Mine/MineCreation.java
@@ -0,0 +1,22 @@
+package com.jetbrains;
+
+public class MineCreation {
+
+    MineralProduction productionDetails;
+
+    void copperTierOne() {
+        new MineSetup("BT1", 100, 2, 5000, 0, 2);
+        productionDetails.copperProduction();
+    }
+
+    void silverTierOne() {
+        new MineSetup("ST1", 1000, 4, 5000, 0, 5);
+        productionDetails.silverProduction();
+    }
+
+    void goldTierOne() {
+        new MineSetup("GT1", 10000, 16, 10000, 0, 20);
+        productionDetails.goldProduction();
+    }
+
+}
