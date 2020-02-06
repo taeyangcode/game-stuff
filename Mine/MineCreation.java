4f9f08e3caadf47a9411ebb21f0d00cd5d515860
태양 <56237265+taeyangcode@users.noreply.github.com>
2020-02-05 16:45:30 -0800

    Update MineCreation.java

--- Mine/MineCreation.java
+++ Mine/MineCreation.java
@@ -1,11 +1,11 @@
-package com.jetbrains;
+package com.company;
 
 public class MineCreation {
 
     MineralProduction productionDetails;
 
     void copperTierOne() {
-        new MineSetup("BT1", 100, 2, 5000, 0, 2);
+        new MineSetup("CT1", 100, 2, 5000, 0, 2);
         productionDetails.copperProduction();
     }
 
