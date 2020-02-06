87766cc18b0b451563e8e1e99e669e94fba35191
태양 <56237265+taeyangcode@users.noreply.github.com>
2020-02-05 16:45:51 -0800

    Update WindowSetup.java

--- Mine/WindowSetup.java
+++ Mine/WindowSetup.java
@@ -2,10 +2,17 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class WindowSetup {
 
+    MineCreation creationDetails;
+    MineToMap mapDetails;
+
     private JFrame gameWindow;
+    private JPanel gamePanel;
 
     private int screenWidth, screenHeight;
 
@@ -17,8 +24,10 @@ public class WindowSetup {
     void frameSetup() {
         gameWindow.setSize(screenWidth, screenHeight);
         gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        gameWindow.setTitle("MvP");
+        gameWindow.setTitle("Mine Game");
         gameWindow.setLocationRelativeTo(null);
+        gameWindow.add(gameWindowSetup(), BorderLayout.NORTH);
+        gameWindow.add(buyCopperMine(), BorderLayout.SOUTH);
     }
 
     void getScreenSize() {
@@ -27,5 +36,31 @@ public class WindowSetup {
         screenWidth = (int) (screenSize.getWidth() * .75);
         screenHeight = (int) (screenSize.getHeight() * .75);
     }
+
+    JPanel gameWindowSetup() {
+        gamePanel.setLayout(new GridLayout(8, 8));
+        return gamePanel;
+    }
+
+    JButton buyCopperMine() {
+        JButton copperBuy = new JButton("Copper Mine");
+        copperBuy.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                gamePanel.add(mapDetails.addMine());
+            }
+        });
+        return copperBuy;
+    }
+
+    /*
+    JButton buySilverMine() {
+
+    }
+
+    JButton buyGoldMine() {
+
+    }
+    */
 
 }
