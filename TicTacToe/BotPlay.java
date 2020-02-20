9f712597648097e688fc09d4f3c751b80b5d0df7
태양 <56237265+taeyangcode@users.noreply.github.com>
2020-02-20 09:48:40 -0800

    Update BotPlay.java

--- TicTacToe/BotPlay.java
+++ TicTacToe/BotPlay.java
@@ -56,11 +56,41 @@ public class BotPlay {
     }
 
     int loseCheckTwo() {
-        if(checkXFilled().contains(0) && checkXFilled().contains(1) && !checkXFilled().contains(2)) {
+        if(checkTwo(0, 1, 2)) {
             return 2;
         }
-        else if(checkXFilled().contains(1) && checkXFilled().contains(2) && !checkXFilled().contains(0)) {
-            return 2;
+        else if(checkTwo(0, 2, 1)) {
+            return 1;
+        }
+        else if(checkTwo(1, 2, 0)) {
+            return 0;
+        }
+        else if(checkTwo(3, 4, 5)) {
+            return 5;
+        }
+        else if(checkTwo(3, 5, 4)) {
+            return 4;
+        }
+        else if(checkTwo(4, 5, 3)) {
+            return 3;
+        }
+        else if(checkTwo(6, 7, 8)) {
+            return 8;
+        }
+        else if(checkTwo(6, 8, 7)) {
+            return 7;
+        }
+        else if(checkTwo(7, 8, 6)) {
+            return 6;
+        }
+    }
+    
+    boolean checkTwo(int fill1, int fill2, int unfill) {
+        if(checkXFilled().contains(num1) && checkXFilled().contains(num2) && !checkXFilled().contains(unfill) && !checkOFilled().contains(unfill)) {
+            return true;
+        }
+        else {
+            return false;
         }
     }
 
